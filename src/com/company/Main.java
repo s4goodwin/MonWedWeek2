78c246f8e0a6241fc45a76169612f061b8c8c9c1
jsantore <jsantore@bridgew.edu>
2021-09-08 14:41:38 -0400

    added second class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var reader = new Scanner(System.in);
+        System.out.print("How old are you:");
+        var age = reader.nextFloat();
+        var dogYears = age * 7;
+        System.out.println("Wow you are "+ age + " that is "+ dogYears+ " in dog years");
     }
 }
