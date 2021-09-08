7e439fb39a3ff3b13460581671410c9fc8fa972b
jsantore <jsantore@bridgew.edu>
2021-09-08 15:02:29 -0400

    end of Monday class

--- src/Example3.java
+++ src/Example3.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Example3 {
-    
+    public static void main(String[] args){
+        var studentNames = new ArrayList<String>();
+        var reader = new Scanner(System.in);
+        var userResponse = "";
+        while (!userResponse.equals("done")){
+            System.out.print("Type in the next student name or 'done' to exit:");
+            userResponse = reader.nextLine();
+            if (userResponse.equals("done")){
+                break;
+            }
+            studentNames.add(userResponse);
+        }
+        System.out.println("All of your students in the class are below:");
+
+    }
 }
